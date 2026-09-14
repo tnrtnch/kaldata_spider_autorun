0aafbbf7c646a6079df8f4e4bc7106f1131bafc2
tnrtnch <tnrtnch@abv.bg>
2026-09-14 13:24:10 +0300

    fix spider

--- kaldata/spiders/kaldata_spdier.py
+++ kaldata/spiders/kaldata_spdier.py
@@ -5,11 +5,36 @@ from kaldata.items import KaldataItem
 class KaldataSpiderSpider(scrapy.Spider):
     name = "kaldata_spider"
     # allowed_domains = ["www.kaldata.com"]
-    # start_urls = ["https://www.kaldata.com"]
+    start_urls = ["https://www.kaldata.com"]
 
-    async def start(self):
-        url = 'https://www.kaldata.com'
-        yield scrapy.Request(url, callback=self.parse)
+    # async def start(self):
+    #     url = 'https://www.kaldata.com'
+    #     yield scrapy.Request(url, callback=self.parse)
+
+    def start_requests(self):
+
+        headers = {
+            "User-Agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/125.0.0.0 Safari/537.36"
+            ),
+            "Accept": (
+                "text/html,application/xhtml+xml,"
+                "application/xml;q=0.9,image/avif,image/webp,"
+                "image/apng,*/*;q=0.8"
+            ),
+            "Accept-Language": "en-US,en;q=0.9",
+            "Referer": "https://www.google.com/",
+            "Upgrade-Insecure-Requests": "1",
+        }
+
+        for url in self.start_urls:
+            yield scrapy.Request(
+                url,
+                headers=headers,
+                callback=self.parse
+            )    
 
     
     def parse(self, response):
